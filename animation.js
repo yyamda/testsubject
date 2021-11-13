b519560e119d65d101834c468e1eedfa084171d9
yyamda <67169750+yyamda@users.noreply.github.com>
2021-11-13 06:41:34 -0800

    message here please

---
+++ animation.js
@@ -0,0 +1,24 @@
+
+const popmotion = require('popmotion');
+
+const { styler, spring, listen, pointer, volue} = popmotion;
+
+const ball = document.querySelector(".box");
+const divStyler = popmotion.styler(ball);
+const ballXY = popmotion.value({ x: 0, y: 0 }, divStyler.set);
+
+popmotion.listen(ball, "mousedown touchstart").start(e => {
+  e.preventDefault();
+  popmotion.pointer(ballXY.get()).start(ballXY);
+});
+
+popmotion.listen(document, "mouseup").start(() => {
+  popmotion
+    .spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200
+    })
+    .start(ballXY);
+});
